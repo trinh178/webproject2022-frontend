9a46ac2967aa8c6fa933f97d9d9bf5fff2b485ba
Trinh Pham <trinh.pham@tekmedi.vn>
2022-03-16 13:06:37 +0700

    update

--- src/libs/ecanvas/circle-eobject.ts
+++ src/libs/ecanvas/circle-eobject.ts
@@ -1,70 +1,77 @@
-import { EObjectOption } from './types';
-import { distance } from 'mathjs';
-import { EObject } from './eobject';
+import { EObjectOption } from "./types";
+import { distance } from "mathjs";
+import { EObject } from "./eobject";
 
 interface CircleOption {
-    radius: number,
-    fillColor?: string,
-    borderColor?: string,
-    borderDashed?: boolean,
-    borderWidth?: number,
-    borderLineDash?: number[],
-} 
+  radius: number;
+  fillColor?: string;
+  borderColor?: string;
+  borderDashed?: boolean;
+  borderWidth?: number;
+  borderLineDash?: number[];
+}
 
 export class CircleEObject extends EObject {
-    circleOption: CircleOption;
+  circleOption: CircleOption;
 
-    constructor(circleOption: CircleOption, option?: EObjectOption) {
-        super(option);
-        this.circleOption = circleOption;
+  constructor(circleOption: CircleOption, option?: EObjectOption) {
+    super(option);
+    this.circleOption = circleOption;
+  }
+
+  onDraw(ctx: CanvasRenderingContext2D): void {
+    super.onDraw(ctx);
+
+    // Reset
+    ctx.setLineDash([]);
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.circleOption.radius, 0, 2 * Math.PI);
+
+    if (this.circleOption.fillColor) {
+      ctx.fillStyle = this.circleOption.fillColor;
+      ctx.fill();
     }
 
-    onDraw(ctx: CanvasRenderingContext2D): void {
-        super.onDraw(ctx);
-
-        // Reset
-        ctx.setLineDash([]);
-
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.circleOption.radius, 0, 2*Math.PI);
-
-        if (this.circleOption.fillColor) {
-            ctx.fillStyle = this.circleOption.fillColor;
-            ctx.fill();
-        }
-
-        if (this.circleOption.borderColor) {
-            ctx.strokeStyle = this.circleOption.borderColor;
-            if (this.circleOption.borderDashed) {
-                ctx.setLineDash(this.circleOption.borderLineDash || [10]);
-            }
-            ctx.lineWidth = this.circleOption.borderWidth || 2;
-            ctx.stroke();
-        }
+    if (this.circleOption.borderColor) {
+      ctx.strokeStyle = this.circleOption.borderColor;
+      if (this.circleOption.borderDashed) {
+        ctx.setLineDash(this.circleOption.borderLineDash || [10]);
+      }
+      ctx.lineWidth = this.circleOption.borderWidth || 2;
+      ctx.stroke();
     }
+  }
 
-    // Methods
-    isCollisionPosition(x: number, y: number): boolean {
-        return distance([this.x, this.y], [x, y]) <= this.circleOption.radius;
-    }
-    getVerticesPosition(): { x: number; y: number; }[] {
-        // TODO: re-calc array points
-        const topLeft = {
-            x: this.x - this.circleOption.radius,
-            y: this.y - this.circleOption.radius,
-        }
-        const topRight = {
-            x: topLeft.x + this.circleOption.radius * 2,
-            y: topLeft.y,
-        }
-        const bottomLeft = {
-            x: topLeft.x,
-            y: topLeft.y + this.circleOption.radius * 2,
-        }
-        const bottomRight = {
-            x: topRight.x,
-            y: bottomLeft.y,
-        }
-        return [ topLeft, topRight, bottomLeft, bottomRight ];
-    }
+  // Methods
+  isCollisionPosition(x: number, y: number): boolean {
+    return (
+      x >= this.x - this.circleOption.radius &&
+      x < this.x + this.circleOption.radius &&
+      y >= this.y - this.circleOption.radius &&
+      y < this.y + this.circleOption.radius
+    );
+    // TODO
+    //return distance([this.x, this.y], [x, y]) <= this.circleOption.radius;
+  }
+  getVerticesPosition(): { x: number; y: number }[] {
+    // TODO: re-calc array points
+    const topLeft = {
+      x: this.x - this.circleOption.radius,
+      y: this.y - this.circleOption.radius,
+    };
+    const topRight = {
+      x: topLeft.x + this.circleOption.radius * 2,
+      y: topLeft.y,
+    };
+    const bottomLeft = {
+      x: topLeft.x,
+      y: topLeft.y + this.circleOption.radius * 2,
+    };
+    const bottomRight = {
+      x: topRight.x,
+      y: bottomLeft.y,
+    };
+    return [topLeft, topRight, bottomLeft, bottomRight];
+  }
 }
